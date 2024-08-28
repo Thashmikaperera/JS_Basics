00975963f1cbeebb510c857080cd3198a001e6e0
Thashmika Perera <pthashmika@gmail.com>
2024-08-28 09:20:35 +0530

    adding JS cases

---
+++ JS/main.js
@@ -0,0 +1,54 @@
+console.log("Hello World");
+
+var name="Thashi";
+var age= 20
+var salary =250000;
+console.log(name);
+console.log(age);
+console.log(salary);
+
+var x=10;
+let y=20;
+const u=10;
+
+console.log(u);
+
+//----------------------------
+var v;
+let c;
+const b=10;  //(Const value should be initialized while declaring it)
+
+
+//------------(let & const variables cannot be redeclare.but can in var variable)----------------
+
+var school;
+var school;
+
+// let car;
+// let car;(error)
+
+// const van="dolphin";
+// const van="dolphin";(error)
+
+//----------------------------
+
+var x =10;
+x=5;//(new value x=5 {var variable can be updated}) 
+
+let s = 20;
+s = 10;//(new value s=10 {let variables can be updated}) 
+
+// const g = 30;
+// g = 20;//(error {cannot update constant variable}) 
+
+
+//----------------------------
+
+var a;
+console.log(a);
+
+let o;
+console.log(o);
+
+const q=10;
+console.log(q);
